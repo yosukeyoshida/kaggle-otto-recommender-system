ac0294494d4eee799458f20fb5014fcf309595f3
yosukeyoshida <y.yoshida0327@gmail.com>
2022-12-02 21:36:42 +0900

    update

--- src/ranker.py
+++ src/ranker.py
@@ -1,9 +1,16 @@
 import gc
+from wandb.lightgbm import wandb_callback, log_summary
+import lightgbm as lgb
+import os
+import wandb
+from sklearn.model_selection import GroupKFold
 import glob
 import pickle
 
 import pandas as pd
-from lightgbm.sklearn import LGBMRanker
+
+class CFG:
+    wandb = True
 
 
 def read_files(path):
@@ -68,62 +75,112 @@ def dump_pickle(path, o):
         pickle.dump(o, f)
 
 
-def main():
-    for type in ["clicks", "carts", "orders"]:
-        train = read_files("./input/lgbm_dataset/*")
-        train_labels_all = read_train_labels()
-        session_length = train.groupby("session").size().to_frame().rename(columns={0: "session_length"}).reset_index()
+def run_train(type, output_dir):
+    train = read_files("./input/lgbm_dataset/*")
+    train_labels_all = read_train_labels()
+    train_labels = train_labels_all[train_labels_all["type"] == type]
+    train_labels["gt"] = 1
+    train = train.merge(train_labels, how="left", on=["session", "aid"])
+    del train_labels_all
+    gc.collect()
+    train["gt"].fillna(0, inplace=True)
+    train["gt"] = train["gt"].astype("int8")
+    train = train.reset_index(drop=True)
+    # print(train.dtypes)
+
+    feature_cols = train.drop(columns=["gt", "session", "type"]).columns.tolist()
+    targets = train["gt"]
+    group = train["session"]
+    train = train[feature_cols + ["session"]]
+
+    kf = GroupKFold(n_splits=5)
+    for fold, (train_indices, valid_indices) in enumerate(kf.split(train, targets, group)):
+        X_train, X_valid = train.loc[train_indices], train.loc[valid_indices]
+        y_train, y_valid = targets.loc[train_indices], targets.loc[valid_indices]
+
+        X_train = X_train.sort_values(["session", "aid"])
+        y_train = y_train.loc[X_train.index]
+        X_valid = X_valid.sort_values(["session", "aid"])
+        y_valid = y_valid.loc[X_valid.index]
+
+        session_length = X_train.groupby("session").size().to_frame().rename(
+            columns={0: "session_length"}).reset_index()
         session_lengths_train = session_length["session_length"].values
-        train = train.merge(session_length, on="session")
-        train["session_length"] = train["session_length"].astype("int16")
+        X_train = X_train.merge(session_length, on="session")
+        X_train["session_length"] = X_train["session_length"].astype("int16")
         del session_length
         gc.collect()
 
-        train_labels = train_labels_all[train_labels_all["type"] == type]
-        train_labels["gt"] = 1
-        train = train.merge(train_labels, how="left", on=["session", "aid"])
-        del train_labels
+        session_length = X_valid.groupby("session").size().to_frame().rename(
+            columns={0: "session_length"}).reset_index()
+        session_lengths_valid = session_length["session_length"].values
+        X_valid = X_valid.merge(session_length, on="session")
+        X_valid["session_length"] = X_valid["session_length"].astype("int16")
+        del session_length
         gc.collect()
-        train["gt"].fillna(0, inplace=True)
-        train["gt"] = train["gt"].astype("int8")
-        print(train.dtypes)
 
-        ranker = LGBMRanker(
-            objective="lambdarank",
-            metric="ndcg",
-            boosting_type="dart",
-            n_estimators=100,
-            importance_type="gain",
-        )
-        feature_cols = train.drop(columns=["gt", "session", "type"]).columns.tolist()
-        targets = train["gt"]
-        train = train[feature_cols]
+        X_train = X_train[feature_cols]
+        # X_valid = X_valid[feature_cols]
 
-        # train
+        params = {
+            'objective': 'lambdarank',
+            'metric': 'ndcg',
+            'boosting_type': "dart",
+            # 'lambdarank_truncation_level': 10,
+            # 'ndcg_eval_at': [10, 5, 20],
+            'n_estimators': 100,
+            'random_state': 42,
+            'importance_type': "gain",
+        }
+        _train = lgb.Dataset(X_train, y_train, group=session_lengths_train)
+        _valid = lgb.Dataset(X_valid[feature_cols], y_valid, reference=_train, group=session_lengths_valid)
+        del X_train, y_train, y_valid, session_lengths_train, session_lengths_valid
+        gc.collect()
         print("train start")
-        ranker = ranker.fit(
-            train,
-            targets,
-            group=session_lengths_train,
-        )
-        print("train finish")
-        dump_pickle(f"output/lgbm/ranker_{type}.pkl", ranker)
-        del train
+        ranker = lgb.train(params, _train, valid_sets=[_valid], callbacks=[wandb_callback()])
+        log_summary(ranker, save_model_checkpoint=True)
+        dump_pickle(os.path.join(output_dir, f"ranker_{type}.pkl"), ranker)
+        X_valid = X_valid.sort_values(["session", "aid"])
+        scores = ranker.predict(X_valid[feature_cols])
+        del ranker
         gc.collect()
+        X_valid["score"] = scores
+        X_valid = X_valid.sort_values(["session", "score"]).groupby("session").tail(20)
+        X_valid = X_valid.groupby("session")["aid"].apply(list).to_frame()
+        train_labels = train_labels.groupby("session")["aid"].apply(list).to_frame()
+        train_labels = train_labels.rename(columns={"aid": "ground_truth"})
+        joined = X_valid.merge(train_labels, how="left", on=["session"])
+        del X_valid, train_labels
+        gc.collect()
+        joined = joined[joined["ground_truth"].notnull()]
+        joined["hits"] = joined.apply(lambda df: len(set(df.aid).intersection(set(df.ground_truth))), axis=1)
+        joined["gt_count"] = joined.ground_truth.str.len().clip(0, 20)
+        joined["recall"] = joined["hits"] / joined["gt_count"]
+        recall = joined["hits"].sum() / joined["gt_count"].sum()
+        break
+    if CFG.wandb:
+        wandb.log({f"{type} recall": recall})
+    return recall
 
-        test = read_files("./input/lgbm_dataset_test/*")
-        session_length = test.groupby("session").size().to_frame().rename(columns={0: "session_length"}).reset_index()
-        test = test.merge(session_length, on="session")
-        for _type in ["clicks", "carts", "orders"]:
-            scores = ranker.predict(test[feature_cols])
-            test["score"] = scores
-            test_predictions = test.sort_values(["session", "score"]).groupby("session").tail(20)
-            test_predictions = test_predictions.groupby("session")["aid"].apply(list)
-            test_predictions = test_predictions.to_frame().reset_index()
-            dump_pickle(f"output/lgbm/test_predictions_{_type}.pkl", test_predictions)
-        del test, ranker
-        gc.collect()
-        print(f"type={type} finish")
+
+def main():
+    run_name = None
+    if CFG.wandb:
+        wandb.init(project="kaggle-otto")
+        run_name = wandb.run.name
+    if run_name is not None:
+        output_dir = os.path.join("output/lgbm", run_name)
+    else:
+        output_dir = "output/lgbm"
+    os.makedirs(output_dir, exist_ok=True)
+
+    clicks_recall = run_train("clicks", output_dir)
+    carts_recall = run_train("carts", output_dir)
+    orders_recall = run_train("orders", output_dir)
+    weights = {"clicks": 0.10, "carts": 0.30, "orders": 0.60}
+    total_recall = clicks_recall * weights["clicks"] + carts_recall * weights["carts"] + orders_recall * weights["orders"]
+    if CFG.wandb:
+        wandb.log({"total recall": total_recall})
 
 
 if __name__ == "__main__":
