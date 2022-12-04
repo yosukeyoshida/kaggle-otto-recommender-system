703418c81b6f41231e73d95e3193e65fc1fc64a9
yosukeyoshida <y.yoshida0327@gmail.com>
2022-12-04 22:20:10 +0900

    revert sql

--- sql/bq.sql
+++ sql/bq.sql
@@ -5,15 +5,14 @@ EXPORT DATA
     format='PARQUET',
     overwrite=true
   )
-AS
-WITH session_stats AS (
+AS WITH session_stats AS (
     SELECT
         session,
         MIN(ts) AS ts_start,
         MAX(ts) AS ts_end,
-        SUM(CASE WHEN type = 'clicks' THEN 1 ELSE 0 END) AS session_clicks_cnt,
-        SUM(CASE WHEN type = 'carts' THEN 1 ELSE 0 END) AS session_carts_cnt,
-        SUM(CASE WHEN type = 'orders' THEN 1 ELSE 0 END) AS session_orders_cnt,
+        SUM(CASE WHEN type = 'clicks' THEN 1 ELSE 0 END) AS clicks_cnt,
+        SUM(CASE WHEN type = 'carts' THEN 1 ELSE 0 END) AS carts_cnt,
+        SUM(CASE WHEN type = 'orders' THEN 1 ELSE 0 END) AS orders_cnt,
         COUNT(*) AS session_interaction_length
     FROM `kaggle-352109.otto.otto-validation-test` -- FIXME
 --     FROM `kaggle-352109.otto.test`
@@ -28,12 +27,12 @@ WITH session_stats AS (
         ts - ts_start AS sec_since_session_start,
         ts_end - ts AS sec_to_session_end,
         session_interaction_length,
-        session_clicks_cnt,
-        session_carts_cnt,
-        session_orders_cnt,
-        session_aid_clicks_cnt,
-        session_aid_carts_cnt,
-        session_aid_orders_cnt,
+        clicks_cnt,
+        carts_cnt,
+        orders_cnt,
+        this_aid_clicks_cnt,
+        this_aid_carts_cnt,
+        this_aid_orders_cnt,
         CASE WHEN session_interaction_length = 1 THEN 1 ELSE POW(2, (0.1 + ((1 - 0.1) / (session_interaction_length - 1)) * (session_interaction_length - action_num_reverse_chrono - 1))) - 1 END AS log_recency_score
     FROM (
         SELECT
@@ -44,13 +43,13 @@ WITH session_stats AS (
             ss.ts_start,
             ss.ts_end,
             t.type,
-            ss.session_clicks_cnt,
-            ss.session_carts_cnt,
-            ss.session_orders_cnt,
+            ss.clicks_cnt,
+            ss.carts_cnt,
+            ss.orders_cnt,
             ss.session_interaction_length,
-            SUM(CASE WHEN t.type = 'clicks' THEN 1 ELSE 0 END) OVER (PARTITION BY t.session, t.aid) AS session_aid_clicks_cnt,
-            SUM(CASE WHEN t.type = 'carts' THEN 1 ELSE 0 END) OVER (PARTITION BY t.session, t.aid) AS session_aid_carts_cnt,
-            SUM(CASE WHEN t.type = 'orders' THEN 1 ELSE 0 END) OVER (PARTITION BY t.session, t.aid) AS session_aid_orders_cnt
+            SUM(CASE WHEN t.type = 'clicks' THEN 1 ELSE 0 END) OVER (PARTITION BY t.session, t.aid) AS this_aid_clicks_cnt,
+            SUM(CASE WHEN t.type = 'carts' THEN 1 ELSE 0 END) OVER (PARTITION BY t.session, t.aid) AS this_aid_carts_cnt,
+            SUM(CASE WHEN t.type = 'orders' THEN 1 ELSE 0 END) OVER (PARTITION BY t.session, t.aid) AS this_aid_orders_cnt
         FROM `kaggle-352109.otto.otto-validation-test` t -- FIXME
 --         FROM `kaggle-352109.otto.test` t
         INNER JOIN session_stats ss ON ss.session = t.session
@@ -66,12 +65,12 @@ WITH session_stats AS (
         sec_since_session_start,
         sec_to_session_end,
         session_interaction_length,
-        session_clicks_cnt,
-        session_carts_cnt,
-        session_orders_cnt,
-        session_aid_clicks_cnt,
-        session_aid_carts_cnt,
-        session_aid_orders_cnt,
+        clicks_cnt,
+        carts_cnt,
+        orders_cnt,
+        this_aid_clicks_cnt,
+        this_aid_carts_cnt,
+        this_aid_orders_cnt,
         log_recency_score,
         (CASE WHEN type = 'clicks' THEN 1 WHEN type = 'carts' THEN 6 WHEN type = 'orders' THEN 3 END) * log_recency_score AS type_weighted_log_recency_score
     FROM aid_list
@@ -80,12 +79,12 @@ WITH session_stats AS (
         session,
         aid,
         session_interaction_length,
-        session_clicks_cnt,
-        session_carts_cnt,
-        session_orders_cnt,
-        session_aid_clicks_cnt,
-        session_aid_carts_cnt,
-        session_aid_orders_cnt,
+        clicks_cnt,
+        carts_cnt,
+        orders_cnt,
+        this_aid_clicks_cnt,
+        this_aid_carts_cnt,
+        this_aid_orders_cnt,
         AVG(action_num_reverse_chrono) AS avg_action_num_reverse_chrono,
         MIN(action_num_reverse_chrono) AS min_action_num_reverse_chrono,
         MAX(action_num_reverse_chrono) AS max_action_num_reverse_chrono,
@@ -106,18 +105,18 @@ WITH session_stats AS (
         NULL AS covisit_orders_candidate_num,
         NULL AS w2v_candidate_num
     FROM aid_list2
-    GROUP BY session, aid, session_interaction_length, session_clicks_cnt, session_carts_cnt, session_orders_cnt, session_aid_clicks_cnt, session_aid_carts_cnt, session_aid_orders_cnt
+    GROUP BY session, aid, session_interaction_length, clicks_cnt, carts_cnt, orders_cnt, this_aid_clicks_cnt, this_aid_carts_cnt, this_aid_orders_cnt
 ), covisit AS (
     SELECT
         session,
         aid,
         session_interaction_length,
-        session_clicks_cnt,
-        session_carts_cnt,
-        session_orders_cnt,
-        NULL AS session_aid_clicks_cnt,
-        NULL AS session_aid_carts_cnt,
-        NULL AS session_aid_orders_cnt,
+        clicks_cnt,
+        carts_cnt,
+        orders_cnt,
+        NULL AS this_aid_clicks_cnt,
+        NULL AS this_aid_carts_cnt,
+        NULL AS this_aid_orders_cnt,
         NULL AS avg_action_num_reverse_chrono,
         NULL AS min_action_num_reverse_chrono,
         NULL AS max_action_num_reverse_chrono,
@@ -143,27 +142,27 @@ WITH session_stats AS (
             t.type,
             t.aid,
             ROW_NUMBER() OVER (PARTITION BY t.session, t.type) AS covisit_type_candidate_num,
-            ss.session_clicks_cnt,
-            ss.session_carts_cnt,
-            ss.session_orders_cnt,
+            ss.clicks_cnt,
+            ss.carts_cnt,
+            ss.orders_cnt,
             ss.session_interaction_length
         FROM `kaggle-352109.otto.covisit` t -- FIXME
 --         FROM `kaggle-352109.otto.covisit_test` t
         INNER JOIN session_stats ss ON ss.session = t.session
         WHERE t.aid is not NULL
     ) t
-    GROUP BY session, aid, session_interaction_length, session_clicks_cnt, session_carts_cnt, session_orders_cnt
+    GROUP BY session, aid, session_interaction_length, clicks_cnt, carts_cnt, orders_cnt
 ), w2v AS (
     SELECT
         session,
         aid,
         session_interaction_length,
-        session_clicks_cnt,
-        session_carts_cnt,
-        session_orders_cnt,
-        NULL AS session_aid_clicks_cnt,
-        NULL AS session_aid_carts_cnt,
-        NULL AS session_aid_orders_cnt,
+        clicks_cnt,
+        carts_cnt,
+        orders_cnt,
+        NULL AS this_aid_clicks_cnt,
+        NULL AS this_aid_carts_cnt,
+        NULL AS this_aid_orders_cnt,
         NULL AS avg_action_num_reverse_chrono,
         NULL AS min_action_num_reverse_chrono,
         NULL AS max_action_num_reverse_chrono,
@@ -188,22 +187,19 @@ WITH session_stats AS (
             t.session,
             t.aid,
             ROW_NUMBER() OVER (PARTITION BY t.session) AS w2v_candidate_num,
-            ss.session_clicks_cnt,
-            ss.session_carts_cnt,
-            ss.session_orders_cnt,
+            ss.clicks_cnt,
+            ss.carts_cnt,
+            ss.orders_cnt,
             ss.session_interaction_length
         FROM `kaggle-352109.otto.w2v` t -- FIXME
 --         FROM `kaggle-352109.otto.w2v_test` t
         INNER JOIN session_stats ss ON ss.session = t.session
         WHERE t.aid is not NULL
     ) t
-    GROUP BY session, aid, session_interaction_length, session_clicks_cnt, session_carts_cnt, session_orders_cnt
+    GROUP BY session, aid, session_interaction_length, clicks_cnt, carts_cnt, orders_cnt
 ), ranking AS (
     SELECT
         aid,
-        clicks_cnt,
-        carts_cnt,
-        orders_cnt,
         RANK() OVER (ORDER BY clicks_cnt DESC) AS clicks_rank,
         RANK() OVER (ORDER BY carts_cnt DESC) AS carts_rank,
         RANK() OVER (ORDER BY orders_cnt DESC) AS orders_rank
@@ -228,12 +224,12 @@ WITH session_stats AS (
         session,
         aid,
         MAX(session_interaction_length) AS session_interaction_length,
-        MAX(session_clicks_cnt) AS session_clicks_cnt,
-        MAX(session_carts_cnt) AS session_carts_cnt,
-        MAX(session_orders_cnt) AS session_orders_cnt,
-        MAX(session_aid_clicks_cnt) AS session_aid_clicks_cnt,
-        MAX(session_aid_carts_cnt) AS session_aid_carts_cnt,
-        MAX(session_aid_orders_cnt) AS session_aid_orders_cnt,
+        MAX(clicks_cnt) AS clicks_cnt,
+        MAX(carts_cnt) AS carts_cnt,
+        MAX(orders_cnt) AS orders_cnt,
+        MAX(this_aid_clicks_cnt) AS this_aid_clicks_cnt,
+        MAX(this_aid_carts_cnt) AS this_aid_carts_cnt,
+        MAX(this_aid_orders_cnt) AS this_aid_orders_cnt,
         MAX(avg_action_num_reverse_chrono) AS avg_action_num_reverse_chrono,
         MAX(min_action_num_reverse_chrono) AS min_action_num_reverse_chrono,
         MAX(max_action_num_reverse_chrono) AS max_action_num_reverse_chrono,
@@ -261,12 +257,12 @@ SELECT
     sa.session,
     sa.aid,
     sa.session_interaction_length,
-    sa.session_clicks_cnt,
-    sa.session_carts_cnt,
-    sa.session_orders_cnt,
-    COALESCE(sa.session_aid_clicks_cnt, 0) AS session_aid_clicks_cnt,
-    COALESCE(sa.session_aid_carts_cnt, 0) AS session_aid_carts_cnt,
-    COALESCE(sa.session_aid_orders_cnt, 0) AS session_aid_orders_cnt,
+    sa.clicks_cnt,
+    sa.carts_cnt,
+    sa.orders_cnt,
+    sa.this_aid_clicks_cnt,
+    sa.this_aid_carts_cnt,
+    sa.this_aid_orders_cnt,
     sa.avg_action_num_reverse_chrono,
     sa.min_action_num_reverse_chrono,
     sa.max_action_num_reverse_chrono,
@@ -286,9 +282,6 @@ SELECT
     sa.covisit_carts_candidate_num,
     sa.covisit_orders_candidate_num,
     sa.w2v_candidate_num,
-    COALESCE(clicks_cnt, 0) AS clicks_cnt,
-    COALESCE(carts_cnt, 0) AS carts_cnt,
-    COALESCE(orders_cnt, 0) AS orders_cnt,
     COALESCE(r.clicks_rank, 1000000) AS clicks_rank,
     COALESCE(r.carts_rank, 1000000) AS carts_rank,
     COALESCE(r.orders_rank, 1000000) AS orders_rank
