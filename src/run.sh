44b83eb62a8c9a7b5d3267ae30e6457bda153eb1
yosukeyoshida <y.yoshida0327@gmail.com>
2023-01-13 23:46:18 +0900

    update

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,1 @@
-python src/ranker.py --input_dir 20230112 --n_folds 1
-python src/ranker.py --input_dir 20230112_2 --n_folds 1
-python src/ranker.py --input_dir 20230112_3 --n_folds 1
-python src/ranker.py --input_dir 20230112_4 --n_folds 1
-python src/ranker.py --input_dir 20230112_5 --n_folds 1
+python src/ranker.py --single_fold --input_dir 20230113
