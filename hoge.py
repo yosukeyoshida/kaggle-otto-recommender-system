b74f742f621f9f682e5129c2c637fa3245f793ce
yosukeyoshida <y.yoshida0327@gmail.com>
2023-01-19 01:38:56 +0900

    update

--- hoge.py
+++ hoge.py
@@ -1,11 +1,20 @@
-# for c in ["clicks", "carts", "orders"]:
-# 	for i in range(35):
-# 		num = i + 1
-# 		print(f"MAX(CASE WHEN day_num = {num} THEN {c}_rank ELSE NULL END) AS {c}_rank_day{num}")
-# 
+def main(case):
+	if case == 1:
+		for c in ["clicks", "carts", "orders"]:
+			for i in range(35):
+				num = i + 1
+				print(f"MAX(CASE WHEN day_num = {num} THEN {c}_rank ELSE NULL END) AS {c}_rank_day{num}")
+	elif case == 2:
+		for c in ["clicks", "carts", "orders"]:
+			for i in range(35):
+				num = i + 1
+				print(f"COALESCE(ais.{c}_rank_day{num}, 1000000) AS {c}_rank_day{num},")
+	elif case == 3:
+		for c in ["clicks", "carts", "orders"]:
+			for i in range(35):
+				num = i + 1
+				print(f'"{c}_rank_day{num}": "int32",')
 
 
-for c in ["clicks", "carts", "orders"]:
-	for i in range(35):
-		num = i + 1
-		print(f"COALESCE(ais.{c}_rank_day{num}, 1000000) AS {c}_rank_day{num},")
+if __name__ == "__main__":
+	main(case=3)
