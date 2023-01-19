41a4943fac2a9a697ea3c05b4ed3f6e9b118c7e4
yosukeyoshida <y.yoshida0327@gmail.com>
2023-01-19 15:29:56 +0900

    update

---
+++ hoge.py
@@ -0,0 +1,5 @@
+for t in ['clicks', 'carts', 'orders']:
+	for i in range(7):
+		num = i+1
+		# print(f"SUM(CASE WHEN d.num = {num}  AND type = '{t}' THEN 1 ELSE 0 END) AS {t}_cnt_day{num},")
+		print(f"MAX(session_aid_{t}_cnt_day{num}) AS session_aid_{t}_cnt_day{num},")
