4cf8fa0d2ab8c0e87e649b1b5444825076a2063e
yosukeyoshida <y.yoshida0327@gmail.com>
2023-01-24 21:46:06 +0900

    update

--- src/lfm_sim.py
+++ src/lfm_sim.py
@@ -1,8 +1,8 @@
 import pandas as pd
+from tqdm import tqdm
+import torch
 import pickle
 import gc
-import math
-import glob
 import numpy as np
 import polars as pl
 import os
@@ -10,42 +10,34 @@ import os
 class CFG:
     input_train_dir = "20230121"
     input_test_dir = "20230121"
+    embedding_size = 16
 
 
 def cos_sim(v1, v2):
     return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
 
-def read_ranker_dataset():
-    train_list = []
-    for type in ["clicks", "carts", "orders"]:
-        print(type)
-        path = f"./input/lgbm_dataset/{CFG.input_train_dir}/{type}/*"
-        train = pl.read_parquet(path).to_pandas()
-        train = train[["session", "aid"]]
-        train_list.append(train)
-        del train
-        gc.collect()
-    df = pd.concat(train_list, axis=0, ignore_index=True)
-    del train_list
-    gc.collect()
-    # path = f"./input/lgbm_dataset_test/{CFG.input_test_dir}/*"
-    # test = pl.read_parquet(path).to_pandas()
-    # test = test[["session", "aid"]]
-    # df = pd.concat([train, test], axis=0)
-    df.drop_duplicates(inplace=True, ignore_index=True)
+def read_ranker_train_dataset(type):
+    path = f"./input/lgbm_dataset/{CFG.input_train_dir}/{type}/*"
+    df = pl.read_parquet(path, columns=["session", "aid"]).to_pandas()
+    return df
+
+
+def read_ranker_test_dataset():
+    path = f"./input/lgbm_dataset_test/{CFG.input_test_dir}/*"
+    df = pl.read_parquet(path, columns=["session", "aid"]).to_pandas()
     return df
 
 
 def read_interactions():
-    train_file_path = "./input/otto-chunk-data-inparquet-format/*_parquet/*"
-    train = pl.read_parquet(train_file_path).to_pandas()
-    session_aids = train.groupby("session")["aid"].apply(list)
+    path = "./input/otto-chunk-data-inparquet-format/*_parquet/*"
+    df = pl.read_parquet(path, columns=["session", "aid"]).to_pandas()
+    session_aids = df.groupby("session")["aid"].apply(list)
     return session_aids
 
 
 def read_item_embeddings():
-    path = f"./input/lightfm/mapped_item_embeddings.pkl"
+    path = "./output/lightfm/components16/mapped_item_embeddings.pkl"
     df = pickle.load(open(path, "rb"))
     return df
 
@@ -55,34 +47,57 @@ def dump_pickle(path, o):
         pickle.dump(o, f)
 
 
+def cosine_similarity(candidates_embeddings, interaction_embeddings):
+    Z = candidates_embeddings
+    B = interaction_embeddings.T
+    Z_norm = torch.linalg.norm(Z, dim=1, keepdim=True)
+    B_norm = torch.linalg.norm(B, dim=0, keepdim=True)
+    return ((Z @ B) / (Z_norm @ B_norm)).T
+
+
+def calc_train_similarity(output_dir):
+    for t in ["clicks", "carts", "orders"]:
+        candidates = read_ranker_train_dataset(type="clicks")
+        candidates_session_aids = candidates.groupby("session")["aid"].apply(list)
+        for c in ["sim_mean", "sim_sum"]:
+            candidates[c] = np.nan
+        session_aids = read_interactions()
+        embeddings = read_item_embeddings()
+
+        print(f"candidates session size: {len(candidates_session_aids)}")
+        for session in tqdm(candidates_session_aids.index.tolist()):
+            interaction_embeddings = torch.concat([torch.tensor(embeddings[aid]) for aid in list(set(session_aids[session]))]).reshape(-1, CFG.embedding_size)
+            candidates_embeddings = torch.concat([torch.tensor(embeddings[aid]) for aid in list(set(candidates_session_aids[session]))]).reshape(-1, CFG.embedding_size)
+            sim = cosine_similarity(candidates_embeddings, interaction_embeddings)
+            candidates.loc[candidates["session"] == session, "sim_mean"] = sim.mean(axis=0).tolist()
+            candidates.loc[candidates["session"] == session, "sim_sum"] = sim.sum(axis=0).tolist()
+        dump_pickle(os.path.join(output_dir, f"candidates_train_{t}.pkl"), candidates)
+
+
+def calc_test_similarity(output_dir):
+    candidates = read_ranker_test_dataset()
+    candidates_session_aids = candidates.groupby("session")["aid"].apply(list)
+    for c in ["sim_mean", "sim_sum"]:
+        candidates[c] = np.nan
+    session_aids = read_interactions()
+    embeddings = read_item_embeddings()
+
+    print(f"candidates session size: {len(candidates_session_aids)}")
+    for session in tqdm(candidates_session_aids.index.tolist()):
+        interaction_embeddings = torch.concat([torch.tensor(embeddings[aid]) for aid in list(set(session_aids[session]))]).reshape(-1, CFG.embedding_size)
+        candidates_embeddings = torch.concat([torch.tensor(embeddings[aid]) for aid in list(set(candidates_session_aids[session]))]).reshape(-1, CFG.embedding_size)
+        sim = cosine_similarity(candidates_embeddings, interaction_embeddings)
+        candidates.loc[candidates["session"] == session, "sim_mean"] = sim.mean(axis=0).tolist()
+        candidates.loc[candidates["session"] == session, "sim_sum"] = sim.sum(axis=0).tolist()
+    dump_pickle(os.path.join(output_dir, f"candidates_train_{t}.pkl"), candidates)
+
+
 def main(output_dir):
-    print("read ranker dataset")
-    train = read_ranker_dataset()
-    for c in ["sim_max", "sim_min", "sim_mean", "sim_sum"]:
-        train.loc[:, c] = np.nan
-    print("read interactions")
-    interactions = read_interactions()
-    print("read item embeddings")
-    item_embeddings = read_item_embeddings()
-    print(f"train shape: {train.shape}")
-    for i in range(len(train)):
-        if i % 100000 == 0:
-            print(i)
-        _train = train.loc[i]
-        session = _train["session"]
-        candidates_aid = _train["aid"]
-        aids = interactions.loc[session]
-        sims = []
-        for interaction_aid in aids:
-            sim = cos_sim(item_embeddings[candidates_aid], item_embeddings[interaction_aid])
-            sims.append(sim)
-        train.loc[i, "sim_max"] = np.max(sims)
-        train.loc[i, "sim_min"] = np.min(sims)
-        train.loc[i, "sim_mean"] = np.mean(sims)
-        train.loc[i, "sim_sum"] = np.sum(sims)
-    dump_pickle(os.path.join(output_dir, "similarity.pkl"), train)
+    calc_test_similarity(output_dir)
+    calc_train_similarity(output_dir)
 
 
 if __name__ == "__main__":
-    output_dir = "output/lightfm"
+    output_dir = "output/lightfm_sim"
+    os.makedirs(output_dir, exist_ok=True)
     main(output_dir=output_dir)
